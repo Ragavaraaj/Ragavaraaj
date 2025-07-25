b6f439a5d2ffeb3e100d3da7a806fc1fea39f222
Ragavaraaj <ragavaraaj@qburst.com>
2025-07-25 10:19:22 +0530

    fix: add assetPrefix and image optimization settings in Next.js configuration

--- site/next.config.ts
+++ site/next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   basePath: process.env.PAGES_BASE_PATH,
+  assetPrefix: process.env.PAGES_BASE_PATH, // assetPrefix requires the trailing slash
+  images: {
+    unoptimized: true, // Disable image optimization for static exports if needed
+  },
 };
 
 export default nextConfig;
